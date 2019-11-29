e011a4f456229f287779cf3e003362ec8e9f9502
Kerchik <olainerul@inbox.ru>
2019-11-29 18:55:15 +0200

    Updates

---
+++ precache-manifest.a237dad5b78c442c857db06d4e98a3af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a027ffed7198050a269d4c1b44b7a605",
+    "url": "/StarWarsWiki/index.html"
+  },
+  {
+    "revision": "26ef6d62ddcd57ecc2da",
+    "url": "/StarWarsWiki/static/css/main.232c9de4.chunk.css"
+  },
+  {
+    "revision": "d6a8cdafa43122127841",
+    "url": "/StarWarsWiki/static/js/2.ae01e0d7.chunk.js"
+  },
+  {
+    "revision": "26ef6d62ddcd57ecc2da",
+    "url": "/StarWarsWiki/static/js/main.697f4c79.chunk.js"
+  },
+  {
+    "revision": "5ce0b5819a77fe938c1e",
+    "url": "/StarWarsWiki/static/js/runtime-main.7530e613.js"
+  },
+  {
+    "revision": "b78faeed8c7dcb78eaed8e6502c331e7",
+    "url": "/StarWarsWiki/static/media/sw.b78faeed.png"
+  }
+]);
